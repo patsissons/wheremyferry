9b2481ceb544e97cc6248fce4fa310c03b06147a
Pat Sissons <patricksissons@gmail.com>
2026-06-05 22:33:07 -0700

    cleaning up elapsed formatting

--- src/lib/components/terminal-sailings/sailing-elapsed.svelte
+++ src/lib/components/terminal-sailings/sailing-elapsed.svelte
@@ -3,23 +3,36 @@
   import PeriodicRefresh from '../periodic-refresh.svelte';
 
   export let timestamp: string | Date | undefined;
+
+  function formatElapsed({ value, unit }: ReturnType<typeof calcSince>) {
+    const parts: string[] = [];
+
+    if (value < 0) {
+      parts.push('in ');
+    }
+
+    if (value) {
+      parts.push(Math.abs(value).toString());
+    }
+
+    if (unit) {
+      parts.push(value ? unit.slice(0, 1) : unit);
+    }
+
+    if (value > 0) {
+      parts.push(' ago');
+    }
+
+    return parts.join('');
+  }
 </script>
 
 {#if timestamp instanceof Date}
   <PeriodicRefresh>
     {@const elapsed = calcSince(timestamp)}
-    {#if elapsed.value < 0}
-      in
-    {/if}
-    {#if elapsed.value}
-      {Math.abs(elapsed.value)}
-    {/if}
-    {#if elapsed.unit}
-      {elapsed.value ? elapsed.unit.slice(0, 1) : elapsed.unit}
-    {/if}
-    {#if elapsed.value > 0}
-      ago
-    {/if}
+    <span class="whitespace-nowrap">
+      {formatElapsed(elapsed)}
+    </span>
     <slot />
   </PeriodicRefresh>
 {/if}
