3cc370822672c8500653e491db268d2687d4ddf6
Pat Sissons <patricksissons@gmail.com>
2026-06-08 12:06:57 -0700

    fixing past sailing opacity

--- src/lib/components/terminal-sailings/terminal-sailing-item.svelte
+++ src/lib/components/terminal-sailings/terminal-sailing-item.svelte
@@ -151,7 +151,7 @@
   {value}
   class="gap-4 rounded-lg border border-muted-foreground bg-muted transition-all hover:bg-muted-foreground/15 dark:hover:bg-muted-foreground/30 {sailing.status ===
   'past'
-    ? 'opacity-75'
+    ? 'opacity-50 hover:opacity-75'
     : ''}"
 >
   <Accordion.Trigger class="px-4 py-2" on:click={handleClick}>
